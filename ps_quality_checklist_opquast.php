2a3c6e71f2e0ca37b659108bc8fdc4c93375c179
Constantin Boulanger <constantin.boulanger@gmail.com>
2020-09-15 21:08:56 +0200

    v1.0.0

--- ps_quality_checklist_opquast.php
+++ ps_quality_checklist_opquast.php
@@ -14,7 +14,7 @@ class Ps_Quality_Checklist_Opquast extends Module
     {
         $this->name = 'ps_quality_checklist_opquast';
         $this->tab = 'front_office_features';
-        $this->version = 1.0;
+        $this->version = '1.0.0';
         $this->author = '<a href="https://constantin-boulanger.fr" target="_blank">Constantin Boulanger</a>';
         $this->need_instance = 0;
         $this->bootstrap = true;
