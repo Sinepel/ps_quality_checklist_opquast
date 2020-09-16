eaa58d977d17e18fd6a28f84681e4f831b6a3d6e
Constantin Boulanger <constantin.boulanger@gmail.com>
2020-09-16 14:50:32 +0200

    Rename JSON files + add author + others little things

--- classes/ChecklistUtility.php
+++ classes/ChecklistUtility.php
@@ -2,31 +2,25 @@
 
 class ChecklistUtilty
 {
-    const OPQUAST_THEMATHIQUES = _PS_MODULE_DIR_ . '/ps_quality_checklist_opquast/data/data-fr-thematiques.json';
-    // const OPQUAST_CONTENT = _PS_MODULE_DIR_ . '/ps_quality_checklist_opquast/data/data-fr-content.json';
-    // const OPQUAST_CHECKLIST = _PS_MODULE_DIR_ . '/ps_quality_checklist_opquast/data/data-fr.json';
-    const OPQUAST = _PS_MODULE_DIR_ . '/ps_quality_checklist_opquast/data/CHECKLIST-OPQUAST-2020.json';
+    const OPQUAST_THEMATHIQUES = _PS_MODULE_DIR_ . '/ps_quality_checklist_opquast/data/data-fr-thematics.json';
+    const OPQUAST = _PS_MODULE_DIR_ . '/ps_quality_checklist_opquast/data/checklist-opquast-v4.json';
 
-    public static function getOPQuastChecklist() 
+    public static function getOPQuastChecklist()
     {
         $get_json = file_get_contents(self::OPQUAST);
         $get_json = json_decode($get_json);
-        // $thema = [];
 
         $get_json = get_object_vars($get_json);
         foreach ($get_json as &$criteria) {
-            // dump($criteria->thema);die;
             $sanitized_tags_array =  $criteria->thema;
-            // $thema = array_unique(array_merge($thema, $sanitized_tags_array));
-            $sanitized_tags_array = array_map(  array('Tools','link_rewrite') , $sanitized_tags_array);
+            $sanitized_tags_array = array_map(array('Tools', 'link_rewrite'), $sanitized_tags_array);
             $criteria->sanitized_tags = implode(' ', $sanitized_tags_array);
             $criteria = get_object_vars($criteria);
         }
-        uasort($get_json, function($a, $b) {
+        uasort($get_json, function ($a, $b) {
             return (int)$a['name_fr'] > (int)$b['name_fr'];
         });
-        // echo json_encode($thema);die;
-        
+
         return $get_json;
     }
 
@@ -58,25 +52,27 @@ class ChecklistUtilty
         ];
     }
 
-    public static function getStatsByStatus( $status = '' ) {
-        if ( empty( $status ) ) {
-                return;
+    public static function getStatsByStatus($status = '')
+    {
+        if (empty($status)) {
+            return;
         }
-        $criteria = self::getOPQuastChecklist(); 
-       
+        
+        $criteria = self::getOPQuastChecklist();
+
         $existing_criteria = array();
         $existing_criteria = self::getCurrentCriterias();
-       
+
         $corresponding_criteria = array();
-        if ( $status === 'nv' ) {
-                return count( $criteria ) - count( $existing_criteria );               
+        if ($status === 'nv') {
+            return count($criteria) - count($existing_criteria);
         } else {
-                foreach ( $existing_criteria as $key => $value ) {
-                        if ( $value === $status ) {
-                                $corresponding_criteria[] = $key;
-                        }
+            foreach ($existing_criteria as $key => $value) {
+                if ($value === $status) {
+                    $corresponding_criteria[] = $key;
                 }
-                return count( $corresponding_criteria );
+            }
+            return count($corresponding_criteria);
         }
-}
+    }
 }
--- ps_quality_checklist_opquast.php
+++ ps_quality_checklist_opquast.php
@@ -7,9 +7,15 @@ if (!defined('_PS_VERSION_')) {
 require_once(_PS_MODULE_DIR_ . 'ps_quality_checklist_opquast/classes/ChecklistUtility.php');
 
 
+/**
+ * @author Constantin Boulanger <constantin.boulanger@gmail.com>
+ */
 class Ps_Quality_Checklist_Opquast extends Module
 {
 
+    /**
+     * {@inheritDoc}
+     */
     public function __construct()
     {
         $this->name = 'ps_quality_checklist_opquast';
@@ -26,8 +32,15 @@ class Ps_Quality_Checklist_Opquast extends Module
         $this->description = $this->l('Checklist qualité web Opquast pour Prestashop');
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function install()
     {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            throw new PrestaShopException($this->l('This module requires PHP 5.4 or higher.'));
+        }
+
         if (!Configuration::hasKey('PSOPQUASTCURRENT')) {
             Configuration::updateValue('PSOPQUASTCURRENT', serialize([]));
         }
@@ -35,17 +48,20 @@ class Ps_Quality_Checklist_Opquast extends Module
         return parent::install();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function postProcess()
     {
         $values = null;
-        if (method_exists('Tools','getAllValues')) {
+        if (method_exists('Tools', 'getAllValues')) {
             $values = Tools::getAllValues();
         } else {
             $values = $_GET + $_POST;
         }
-        
-        $opquastResponses = array_filter($values, function ($e) {
-            return false !== strpos($e, 'opquast-checklist-');
+
+        $opquastResponses = array_filter($values, function ($value) {
+            return false !== strpos($value, 'opquast-checklist-');
         }, ARRAY_FILTER_USE_KEY);
 
         if (empty($opquastResponses)) {
@@ -61,6 +77,9 @@ class Ps_Quality_Checklist_Opquast extends Module
         return Configuration::updateValue('PSOPQUASTCURRENT', serialize($toSave));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getContent()
     {
         $html = null;
@@ -84,9 +103,9 @@ class Ps_Quality_Checklist_Opquast extends Module
         $themes = ChecklistUtilty::getThemesFromJSON();
         $currentCriterias = ChecklistUtilty::getCurrentCriterias();
         $stats = ChecklistUtilty::getStats();
-        
+
         $this->context->smarty->assign(
-            array(
+            [
                 'iso_code' => $iso_code,
                 'themes' => $themes,
                 'checklist' => $checklist,
@@ -94,7 +113,7 @@ class Ps_Quality_Checklist_Opquast extends Module
                 'path' => $this->_path,
                 'stats' => $stats,
                 'actionUrl' => $this->context->link->getAdminLink('AdminModules', false) . '&configure=' . $this->name . '&tab_module=' . $this->tab . '&module_name=' . $this->name . '&token=' . Tools::getAdminTokenLite('AdminModules'),
-            )
+            ]
         );
 
         return $html . $this->display(__FILE__, 'views/templates/admin/index.tpl');
